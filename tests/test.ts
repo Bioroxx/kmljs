91e0a5169aad54ece9436ed98ff3e53a8e95af00
Bioroxx <bioroxx@outlook.com>
2024-02-09 19:35:30 +0100

    #3 wip

--- tests/test.ts
+++ tests/test.ts
@@ -1,5 +1,5 @@
 import {readFileSync} from 'fs';
-import {Kml, KMLParser, Placemark} from '../src';
+import {Kml, KMLParser, LookAt, Placemark, Point} from '../src';
 import {get} from 'lodash';
 
 
@@ -19,8 +19,23 @@ test('Parses simple placemark', () => {
   expect(actual).toHaveProperty('hint');
   expect(actual!.hint).toBe(get(expected, 'kml.@_hint'));
   expect(actual!.networkLinkControl).toBeUndefined();
-
   expect(actual!.feature).toBeInstanceOf(Placemark);
-  expect(actual!.feature!.name).toBe(get(expected, 'kml.Placemark.name'));
-  expect(actual!.feature!.description).toBe(get(expected, 'kml.Placemark.description'));
-})
+
+  const placemark = actual!.feature as Placemark;
+
+  expect(placemark.name).toBe(get(expected, 'kml.Placemark.name'));
+  expect(placemark.description).toBe(get(expected, 'kml.Placemark.description'));
+  expect(placemark.view).toBeInstanceOf(LookAt);
+  expect(placemark.geometry).toBeInstanceOf(Point);
+
+  const lookAt = placemark.view as LookAt;
+  const point = placemark.geometry as Point;
+
+  expect(lookAt.longitude).toBe(get(expected, 'kml.Placemark.LookAt.longitude'));
+  expect(lookAt.latitude).toBe(get(expected, 'kml.Placemark.LookAt.latitude'));
+  expect(lookAt.range).toBe(get(expected, 'kml.Placemark.LookAt.range'));
+  expect(lookAt.tilt).toBe(get(expected, 'kml.Placemark.LookAt.tilt'));
+  expect(lookAt.heading).toBe(get(expected, 'kml.Placemark.LookAt.heading'));
+
+  expect(point.coordinates![0]).toBe(get(expected, 'kml.Placemark.Point.coordinates'));
+});
